f974ff5ed1b241211a6dc14443a22ea83d85d6bd
JasonYJWei <68941015+JasonYJWei@users.noreply.github.com>
2023-04-23 15:44:25 -0500

    Create Motif.Analysis.cmd

---
+++ 2.YBX1.eCLIP-seq/Motif.Analysis.cmd
@@ -0,0 +1,2 @@
+# homer/4.10
+findMotifsGenome.pl "$path"/"$package"/10/"$mergedFileName".sig0.05.top1000.bed ~/bwa-index/"$ref" "$path"/"$package"/10/"$mergedFileName".sig0.05.top1000.motifAnalysis/ -norevopp -len 4,6,8,10,12
